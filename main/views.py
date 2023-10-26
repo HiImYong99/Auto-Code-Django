0ea59038faa3f54500b42504c71c8446a7c5a8f3
yongtae.kim <good19422@naver.com>
2023-10-27 00:46:04 +0900

    ✨23.10.26 main 앱 생성

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+
+class IndexView(TemplateView):
+    template_name = 'main/index.html'
+
+
+class LandingView(TemplateView):
+    template_name = 'main/landing.html'
