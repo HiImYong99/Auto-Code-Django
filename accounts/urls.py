7b526a96bc200bed540aa3783412b6c172e501a1
yongtae.kim <good19422@naver.com>
2023-10-31 22:22:01 +0900

    ✨23.10.31 내정보, 회원정보 수정 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path('register/', views.UserCreateView.as_view(), name='signup'),
     path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', views.logout, name='logout'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('edit/profile/', views.EditProfileView.as_view(), name='edit_profile'),
+    path('edit/password/', views.EditProfileView.as_view(), name='edit_password'),
+    # path('profile/delete/', views.profile_delete_view, name='profile_delete'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,21 @@
 from django.conf import settings
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.contrib.auth.views import LoginView, LogoutView
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserChangeForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView, TemplateView
+from accounts.models import User
+from .forms import SignupForm, ChangeUserForm
+from board.models import Post, Comment
+from django.contrib.auth.mixins import LoginRequiredMixin
+# from .forms import CheckPasswordForm
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
 
 
 class UserCreateView(CreateView):
-    form_class = UserCreationForm
+    form_class = SignupForm
     template_name = 'accounts/signup_form.html'
     success_url = settings.LOGOUT_URL
 
@@ -29,5 +36,42 @@ logout = LogoutView.as_view(
 )
 
 
-def profile(request):
-    return render(request, 'accounts/profile.html')
+class ProfileView(TemplateView, LoginRequiredMixin):
+    model = User
+    template_name = 'accounts/profile.html'
+    success_url = reverse_lazy('accounts:profile')
+    paginate_by = 1
+
+    def get_object(self):
+        return self.request.user
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = Post.objects.filter(writer=self.request.user)
+        context['comments'] = Comment.objects.filter(writer=self.request.user)
+        return context
+
+
+class EditProfileView(UpdateView, LoginRequiredMixin):
+    form_class = ChangeUserForm
+    model = User
+    template_name = 'accounts/edit_profile.html'
+    success_url = reverse_lazy('accounts:edit_profile')
+
+    def get_object(self):
+        return self.request.user
+
+
+@login_required
+def profile_delete_view(request):
+    if request.method == 'POST':
+        password_form = CheckPasswordForm(request.user, request.POST)
+
+        if password_form.is_valid():
+            request.user.delete()
+            logout(request)
+            return redirect('/users/login/')
+    else:
+        password_form = CheckPasswordForm(request.user)
+
+    return render(request, 'accounts/profile_delete.html', {'password_form': password_form})
