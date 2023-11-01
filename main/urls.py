c8c1a4fb82aaa6c55fc6b76d3b61ec8bed6c374d
yongtae.kim <good19422@naver.com>
2023-11-01 15:15:05 +0900

    ✨23.11.01 내 기록 페이지 생성

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'main'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
+    path('myhistory/', views.HistoryView.as_view(), name='myhistory'),
 ]
--- main/views.py
+++ main/views.py
@@ -5,3 +5,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'main/index.html'
+
+
+class HistoryView(TemplateView):
+    template_name = 'main/myhistory.html'
