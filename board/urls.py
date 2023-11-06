a58505a15a91d56f3af076f21264b021554825cb
yongtae.kim <good19422@naver.com>
2023-11-06 14:03:18 +0900

    ♻️ 23.11.06 댓글작성 FBV -> CBV

--- board/urls.py
+++ board/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('post/edit/<int:pk>/', views.postupdate, name='postupdate'),
     path('post/delete/<int:pk>/', views.post_delete, name='postdelete'),
     path('post/<int:pk>/likes/', views.likes, name='likes'),
-    path('<int:pk>/comment/new/', views.comment_new, name='comment_new'),
+    path('<int:pk>/comment/new/',
+         views.PostCommentView.as_view(), name='comment_new'),
     path('<int:pk>/comment/delete/', views.comment_delete, name='comment_delete'),
 ]
