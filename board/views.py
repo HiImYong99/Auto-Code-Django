5171be0fe9d2a067bf2a2bfa2df923b7b50cbdfa
yongtae.kim <good19422@naver.com>
2023-11-06 13:59:52 +0900

    ✨23.11.06 정렬 기능 및 최신 인기글 구현

--- board/views.py
+++ board/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.decorators import login_required
@@ -8,6 +8,8 @@ from .models import Post, Comment
 from .forms import PostForm, CommentForm
 from django.db.models import Q
 from datetime import date
+from django.db.models import Count
+
 # Create your views here.
 
 
@@ -25,8 +27,21 @@ class PostListView(ListView):
         qs = super().get_queryset()
         q = self.request.GET.get('q', '')
         c = self.request.GET.get('c', '')
+        s = self.request.GET.get('s', '')
         if q or c:
             qs = qs.filter(Q(title__icontains=q) & Q(category__icontains=c))
+        if s:
+            if s == 'desc':
+                return Post.objects.all().order_by('-id')
+            elif s == 'asc':
+                return Post.objects.all().order_by('id')
+            elif s == 'like':
+                return Post.objects.annotate(like_count=Count('like_user')).order_by('-like_count')
+            elif s == 'view':
+                return Post.objects.all().order_by('-view_count')
+            else:
+                return Post.objects.all().order_by('-id')
+
         return qs
 
 
@@ -36,7 +51,7 @@ postlist = PostListView.as_view()
 class PopularPostListView(ListView):
     model = Post
     ordering = '-id'
-    paginate_by = 5
+    paginate_by = 3
     template_name = 'board/popular_post_list.html'
 
     def get_context_data(self, **kwargs):
@@ -65,11 +80,6 @@ class PostDetailView(DetailView):
         return context
 
     def get_object(self, queryset=None):
-        '''
-        말 그대로 PostDetailView.as_view()에서 사용할 object를 반환합니다.
-        반환 하는데 object를 변경할 수 있는 함수입니다.
-        여기서 원하는 쿼리셋이나 object를 추가한 후 템플릿으로 전달할 수 있습니다.
-        '''
         pk = self.kwargs.get('pk')
         post = Post.objects.get(pk=pk)
         post.view_count += 1
@@ -86,9 +96,9 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     success_url = reverse_lazy('board:postlist')
 
     def form_valid(self, form):
-        post = form.save(commit=False)  # commit=False는 DB에 저장하지 않고 객체만 반환
+        post = form.save(commit=False)
         post.writer = self.request.user
-        return super().form_valid(form)  # 이렇게 호출했을 때 저장합니다.
+        return super().form_valid(form)
 
     def handle_no_permission(self):
         return HttpResponseRedirect(reverse_lazy('accounts:login'))
@@ -126,25 +136,22 @@ class PostDeleteView(UserPassesTestMixin, DeleteView):
 post_delete = PostDeleteView.as_view()
 
 
+class PostCommentView(CreateView, LoginRequiredMixin):
+    model = Post
+    form_class = CommentForm
+
+    def form_valid(self, form):
+        commentForm = form.save(commit=False)
+        commentForm.post = self.get_object()
+        commentForm.writer = self.request.user
+        commentForm.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('board:postdetail', args=[self.get_object().pk])
+
+
 @login_required
-def comment_new(request, pk):
-    post = Post.objects.get(pk=pk)
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            # commit=False는 DB에 저장하지 않고 객체만 반환
-            comment = form.save(commit=False)
-            comment.post = post
-            comment.writer = request.user
-            comment.save()
-            return redirect('board:postdetail', pk)
-    else:
-        form = CommentForm()
-    return render(request, 'board/post_form.html', {
-        'form': form,
-    })
-
-
 def comment_delete(request, pk):
     comment = get_object_or_404(Comment, pk=pk)
     post_id = comment.post.id
